31fbdbbb95e14405ae05480ba83b6ad5bbc41eda
farhan.hassan1 <farhan193.m@gmail.com>
2022-02-10 15:09:16 +0500

    automation framework

---
+++ src/main/java/facrory/DriverFactory.java
@@ -0,0 +1,11 @@
+package facrory;
+
+import org.openqa.selenium.WebDriver;
+
+public class DriverFactory {
+    public WebDriver driver;
+
+    public WebDriver setDriver() {
+        return driver;
+    }
+}
