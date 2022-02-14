88b75bb83d902a214cba310ae6dc708ae5dd9e29
farhan.hassan1 <farhan193.m@gmail.com>
2022-02-14 17:15:24 +0500

    new browser add

--- src/main/java/facrory/DriverFactory.java
+++ src/main/java/facrory/DriverFactory.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
 
 // automation framework
 public class DriverFactory {
@@ -25,9 +26,11 @@ public WebDriver getDriver() {return driver;}
         } else if (browser.equalsIgnoreCase("edge")) {
             WebDriverManager.edgedriver().setup();
             driver = new EdgeDriver();
-
         }
-
+        else if (browser.equalsIgnoreCase("opera")) {
+            WebDriverManager.operadriver().setup();
+            driver = new OperaDriver();
+        }
     }
 }
 
