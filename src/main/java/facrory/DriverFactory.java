2ff6fcbe86af5ac5952d7c300e7715d877a3e459
farhan.hassan1 <farhan193.m@gmail.com>
2022-02-14 17:10:04 +0500

    new changes

--- src/main/java/facrory/DriverFactory.java
+++ src/main/java/facrory/DriverFactory.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-
+// automation framework
 public class DriverFactory {
 
     WebDriver driver;
