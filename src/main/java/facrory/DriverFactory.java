29ccec492bacd813367f87f197198de82914a2c3
farhan.hassan1 <farhan193.m@gmail.com>
2022-02-14 17:06:58 +0500

    new changes

--- src/main/java/facrory/DriverFactory.java
+++ src/main/java/facrory/DriverFactory.java
@@ -1,11 +1,34 @@
 package facrory;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 public class DriverFactory {
-    public WebDriver driver;
 
-    public WebDriver setDriver() {
-        return driver;
+    WebDriver driver;
+public WebDriver getDriver() {return driver;}
+
+    public void setDriver(String browser) {
+
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+
+        } else if (browser.equalsIgnoreCase("edge")) {
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+
+        }
+
     }
 }
+
+
